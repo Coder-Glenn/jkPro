e2b778afa6b87f53484e8408b1cc9d91a7e89e90
troublemaker <SpecialCoder_zh@163.com>
2017-04-22 18:43:38 +0800

    Modify condes style

--- src/cn/graduation/common/db/AppContext.java
+++ src/cn/graduation/common/db/AppContext.java
@@ -4,23 +4,23 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppContext {
-	private static AppContext instance;
-	private static String[] paths = {"beans.xml"};
-	private AbstractApplicationContext appContext;
+    private static AppContext instance;
+    private static String[] paths = {"beans.xml"};
+    private AbstractApplicationContext appContext;
 
-	public synchronized static AppContext getInstance() {
-		if (instance == null) {
-			instance = new AppContext();
-		}
-		return instance;
-	}
+    public synchronized static AppContext getInstance() {
+        if (instance == null) {
+            instance = new AppContext();
+        }
+        return instance;
+    }
 
-	private AppContext() {
-		this.appContext = new ClassPathXmlApplicationContext(paths);
-	}
+    private AppContext() {
+        this.appContext = new ClassPathXmlApplicationContext(paths);
+    }
 
-	public AbstractApplicationContext getAppContext() {
-		return appContext;
-	} 
+    public AbstractApplicationContext getAppContext() {
+        return appContext;
+    } 
 
 }
